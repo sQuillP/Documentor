3bc288fb53e819cac10070208b504652f0f3f243
unknown <willpattisons@yahoo.com>
2022-08-18 23:26:08 -0500

    added autocomplete feature

--- client/src/app/documents/newdoc-overlay/newdoc-overlay.component.ts
+++ client/src/app/documents/newdoc-overlay/newdoc-overlay.component.ts
@@ -1,5 +1,7 @@
 import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
 import { DocumentService } from 'src/app/services/document.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-newdoc-overlay',
@@ -11,18 +13,39 @@ export class NewdocOverlayComponent implements OnInit, AfterViewInit, OnDestroy 
   @ViewChild('overlayRef') overlayRef:ElementRef;
   @ViewChild('inputRef') inputRef:ElementRef;
 
+
+  fetchedEmails$:Observable<any>;
+
+  team:string[] = [];
+  roles:any[] = [];
+
+  searchTimeout:any;
   addedMembers:boolean = false;
   showMemberResults:boolean = false;
+  isFetchingEmails:boolean = false;
   permissionsList:string[] = ['read','modify','admin'];
   searchValue:string = "";
   documentTitle:string = "";
+  
+
+  /**
+   * team: ["id1", "id2", ..., "id12"]
+   * roles:[
+   * {
+   *   user: userid,
+   *   access: "admin" 
+   * }
+   * ]
+   * 
+   */
 
 
   listener:()=>void;
   
   constructor(
     private renderer:Renderer2,
-    private documentService:DocumentService
+    private documentService:DocumentService,
+    private userService:UserService,
     ) { }
 
 
@@ -35,12 +58,25 @@ export class NewdocOverlayComponent implements OnInit, AfterViewInit, OnDestroy 
       });
   }
 
-  onAddTeamMember():void{
-    console.log('clicked')
+  /**Work on adding team members and allowing modification of roles through UI!!! */
+  onAddTeamMember(member:any):void{
+    this.team.push(member._id);
+    this.roles.push({user:member._id, access: "readonly"});
   }
 
   onSearchMembers():void{
-    console.log("should be searching members")
+    // display query loading screen.
+    if(!this.searchValue || this.searchTimeout){
+      clearTimeout(this.searchTimeout);
+      this.isFetchingEmails = false;
+    }
+    this.isFetchingEmails = true;
+    this.searchTimeout = setTimeout(()=> {
+      console.log('fetching data');
+      this.fetchedEmails$ = this.userService.searchUsersByEmail(this.searchValue);
+      this.isFetchingEmails = false;
+      // remove query screen
+    },500)
   }
 
   ngOnInit(): void {
---
+++ client/src/app/services/user.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { map, Observable, tap } from "rxjs";
+
+
+@Injectable({providedIn:"root"})
+export class UserService {
+
+    private ROUTE:string = "http://localhost:5000/api/v1/users";
+
+    constructor(private http:HttpClient){
+
+    }
+
+
+    searchUsersByEmail(expression:string):Observable<any>{
+        return this.http.get(`${this.ROUTE}`,{
+            params:{
+                email: expression
+            }
+        })
+        .pipe(
+            map((response:any) => {
+                return response.data;
+            }),
+            tap(
+                data=> console.log(data)
+            )
+        );
+    }
+}
